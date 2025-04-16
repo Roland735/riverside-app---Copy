c87409966df2441fb881f39986c3fc3788864bc3
Roland735 <85491901+Roland735@users.noreply.github.com>
2025-04-16 10:29:41 +0200

    Update CanteenDashboard.js

--- src/app/dashboard/canteen/CanteenDashboard.js
+++ src/app/dashboard/canteen/CanteenDashboard.js
@@ -72,7 +72,7 @@ const CanteenDashboard = () => {
             <div className="max-w-7xl mx-auto">
                 <h1 className="text-3xl font-bold text-gray-900 mb-8 flex items-center gap-2">
                     <FaClipboardList className="text-blue-600" />
-                    Canteen Dashboards
+                    Welcome Canteen Dashboards
                 </h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {Object.keys(groupedOrders).map((itemName) => {
